0adb5fcbd232b00ca1d0ba5819e51e000037724e
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-10-03 22:49:43 -0500

    Feat: aplicaion de singleton en Biblioteca

--- src/main/java/edu/iudigital/Biblioteca.java
+++ src/main/java/edu/iudigital/Biblioteca.java
@@ -1,18 +1,28 @@
 package edu.iudigital;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Biblioteca {
+    private static Biblioteca instancia;
     private String nombre;
     private List<Libro> catalogo;
     private List<Usuario> usuarios;
 
-    public Biblioteca(String nombre, List<Usuario> usuarios, List<Libro> catalogo) {
+    private Biblioteca(String nombre) {
         this.nombre = nombre;
-        this.usuarios = usuarios;
-        this.catalogo = catalogo;
+        this.catalogo = new ArrayList<>();
+        this.usuarios = new ArrayList<>();
     }
+
+    public static Biblioteca getInstance(String nombre){
+        if(instancia == null){
+            instancia = new Biblioteca(nombre);
+        }
+        return instancia;
+    }
+
 
     public void agregarLibro(Libro libro) {
         catalogo.add(libro);
@@ -20,13 +30,11 @@ public class Biblioteca {
     public List<Libro> buscarPorTitulo(String titulo){
         return catalogo.stream().filter(libro -> libro.getTitulo().equalsIgnoreCase(titulo))
                 .collect(Collectors.toList());
-
-
     }
     public void agregarUsuario(Usuario usuario){
         usuarios.add(usuario);
-
     }
+
 
 
 }
