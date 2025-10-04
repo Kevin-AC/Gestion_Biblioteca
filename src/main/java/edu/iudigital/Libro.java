fa6fd0e2ee5e1e68b5e6276e2a22f250acfd8520
Carolina-H-8 <astrid.hernandez@est.iudigital.edu.co>
2025-10-03 22:27:58 -0500

    Add: Creacion de la clase Libro con atributos.

---
+++ src/main/java/edu/iudigital/Libro.java
@@ -0,0 +1,92 @@
+package edu.iudigital;
+
+public class Libro {
+    protected String titulo;
+    protected String autor;
+    protected String isbn;
+    protected String editorial;
+    protected String anioPublicacion;
+    protected double precio;
+    protected String genero;
+
+
+
+     public Libro (String titulo, String autor, String isbn, String editorial, String anioPublicacion, double precio, String genero){
+         this.titulo = titulo;
+         this.autor = autor;
+         this.isbn = isbn;
+         this.editorial = editorial;
+         this.anioPublicacion = anioPublicacion;
+         this.precio = precio;
+         this.genero = genero;
+     }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(String anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    @Override
+
+    public String toString(){
+         return "Libro" +
+                 "titulo='" + titulo + '\'' +
+                 ", autor='" + autor + '\'' +
+                 ", isbn='" + isbn + '\'' +
+                 ", editorial='" + editorial + '\'' +
+                 ", añoPublicacion=" + anioPublicacion +
+                 ", precio=" + precio +
+                 ", genero='" + genero + '\'';
+
+    }
+}
