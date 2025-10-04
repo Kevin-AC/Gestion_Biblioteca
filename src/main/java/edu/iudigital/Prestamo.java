4b1931d505ec5cbfe7b639d6c579ff3425f49b4a
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-10-04 00:19:55 -0500

    Fix:toString, metodo fechaDevolucion

--- src/main/java/edu/iudigital/Prestamo.java
+++ src/main/java/edu/iudigital/Prestamo.java
@@ -6,14 +6,14 @@ public class Prestamo {
     private Libro libro;
     private Usuario usuario;
     private String fechaPrestamo;
-    private String fehcaDevolucion;
+    private String fechaDevolucion;
     private boolean devuelto;
 
-    public Prestamo(Libro libro, Usuario usuario, String fechaPrestamo, String fehcaDevolucion) {
+    public Prestamo(Libro libro, Usuario usuario, String fechaPrestamo, String fechaDevolucion) {
         this.libro = libro;
         this.usuario = usuario;
         this.fechaPrestamo = fechaPrestamo;
-        this.fehcaDevolucion = fehcaDevolucion;
+        this.fechaDevolucion = fechaDevolucion;
         this.devuelto = false;
     }
 
@@ -37,11 +37,11 @@ public class Prestamo {
     public void setFechaPrestamo(String fechaPrestamo) {
         this.fechaPrestamo = fechaPrestamo;
     }
-    public String getFehcaDevolucion() {
-        return fehcaDevolucion;
+    public String getFechaDevolucion() {
+        return fechaDevolucion;
     }
-    public void setFehcaDevolucion(String fehcaDevolucion) {
-        this.fehcaDevolucion = fehcaDevolucion;
+    public void setFechaDevolucion(String fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
     }
     public boolean isDevuelto() {
         return devuelto;
@@ -51,13 +51,14 @@ public class Prestamo {
     }
     @Override
     public String toString() {
-        return "Prestamo{" +
-                "libro=" + libro.getTitulo() +
-                ", usuario=" + usuario.getNombreCompleto() +
-                ", fechaPrestamo=" + fechaPrestamo +
-                ", devuelto=" + devuelto +
-                '}';
+        return "Préstado:\n" +
+                "  Libro: " + libro.getTitulo() + "\n" +
+                "  Usuario: " + usuario.getNombreCompleto() + "\n" +
+                "  Fecha de Préstamo: " + fechaPrestamo + "\n" +
+                "  Fecha de Devolución: " + (fechaDevolucion != null ? fechaDevolucion : "No devuelto aún") + "\n" +
+                "  Estado: " + (devuelto ? "Devuelto" : "En préstamo");
     }
+
 
 
 }
