2e2a402f7a2c32228a619d2fea8af155e72d1668
Carolina-H-8 <astrid.hernandez@est.iudigital.edu.co>
2025-10-04 00:23:24 -0500

    Add: Creacion de la subclase LibroElectronico.

---
+++ src/main/java/edu/iudigital/LibroElectronico.java
@@ -0,0 +1,36 @@
+package edu.iudigital;
+
+public class LibroElectronico extends Libro{
+    private String formatoLibro;
+    private String url;
+
+    public LibroElectronico (String titulo, String autor,String isbn, String editorial, String anioPublicacion, String genero,
+                             String formatoLibro, String url){
+        super (titulo, autor, isbn, editorial, anioPublicacion, genero);
+        this.formatoLibro = formatoLibro;
+        this.url = url;
+    }
+
+    public String getFormatoLibro() {
+        return formatoLibro;
+    }
+
+    public void setFormatoLibro(String formatoLibro) {
+        this.formatoLibro = formatoLibro;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    @Override
+    public String toString(){
+        return super.toString() + " | LibroElectronico"+
+        "formato del libro='" + formatoLibro + '\'' +
+                ", urlDescarga='" + url+ '\'' +
+                '}';
+    }
+}
