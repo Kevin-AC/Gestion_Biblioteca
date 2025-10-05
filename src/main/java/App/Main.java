ab2fc773ae9565f467cdc388c907d8c6256d011a
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-10-04 22:46:18 -0500

    Refactor:Limpieza de codigo creacion de metodos ejecutarOpciones,leerOpcion

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -8,52 +8,23 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
     static Scanner scanner = new Scanner(System.in);
-    static Biblioteca biblioteca = Biblioteca.getInstance("Biblioteca Central");
-    static BibliotecaService service = new BibliotecaService(biblioteca,scanner);
+    static Biblioteca biblioteca;
+    static BibliotecaService service;
     public static void main(String[] args) {
-        biblioteca.agregarLibro(new Libro("Fudamentos de java","Herbert Schildt","9788426723987","Marcombo","2011","Educativo"));
-        biblioteca.agregarLibro(new Libro("Alas de sangre","Rebecca Yarros","97860739909876","Planeta","2023","Fantasia"));
+        biblioteca = Biblioteca.getInstance("Biblioteca Central");
+        service = new BibliotecaService(biblioteca, scanner);
+        datosDeprueba();
+
         int opcion =0;
         while (opcion != 9){
-            mostarMenu();
-            try {
-                opcion= scanner.nextInt();
-                scanner.nextLine();
-                switch (opcion){
-                    case 1:
-                        service.crearUsuario();
-                        break;
-                    case 2:
-                        service.crearLibro();
-                        break;
-                    case 3:
-                        service.prestarLibro();
-                        break;
-                    case 4:
-                        biblioteca.mostrarLibros();
-                        break;
-                    case 5:
-                        service.devolverLibro();
-                        break;
-                    case 6:
-                        service.listarPrestamos();
-                        break;
-                    case 9:
-                        System.out.println("Saliendo...");
-                        break;
-                    default:
-                        System.out.println("Opcion no valida");
-                }
+            mostrarMenu();
+            opcion= leerOpcion();
+            ejecutarOpciones(opcion);
 
-            }catch (InputMismatchException e){
-                System.out.println("Error: Caracter no valido");
-                scanner.nextLine();
-
-            }
         }
 
     }
-    public static void mostarMenu(){
+    public static void mostrarMenu(){
         System.out.println("Bienvenido a la biblioteca "+ biblioteca.getNombre());
         System.out.println("1. Crear usuario");
         System.out.println("2. Crear libro");
@@ -62,5 +33,48 @@ public class Main {
         System.out.println("5. Devolver libro");
         System.out.println("6. Lista de prestamos");
         System.out.println("9. salir");
+    }
+    public static void datosDeprueba(){
+        biblioteca.agregarLibro(new Libro("Fudamentos de java","Herbert Schildt","9788426723987","Marcombo","2011","Educativo"));
+        biblioteca.agregarLibro(new Libro("Alas de sangre","Rebecca Yarros","97860739909876","Planeta","2023","Fantasia"));
+    }
+    public static int leerOpcion(){
+        int opcion = 0;
+        try {
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+        }catch (InputMismatchException e ){
+            System.out.println("Error: Caracter no valido");
+            scanner.nextLine(); // Limpiar buffer
+        }
+        return opcion;
+    }
+    public static void ejecutarOpciones(int opcion){
+        switch (opcion){
+            case 1:
+                service.crearUsuario();
+                break;
+            case 2:
+                service.crearLibro();
+                break;
+            case 3:
+                service.prestarLibro();
+                break;
+            case 4:
+                biblioteca.mostrarLibros();
+                break;
+            case 5:
+                service.devolverLibro();
+                break;
+            case 6:
+                service.listarPrestamos();
+                break;
+            case 9:
+                System.out.println("Saliendo...");
+                break;
+            default:
+                System.out.println("Opcion no valida");
+        }
+
     }
 }
