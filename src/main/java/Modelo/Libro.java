6bec6b4eb012f255b9963cbfb031bfc5db07caaa
Carolina-H-8 <astrid.hernandez@est.iudigital.edu.co>
2025-10-04 23:19:57 -0500

    chore: Organizando la clase libro electronico.

--- src/main/java/Modelo/Libro.java
+++ src/main/java/Modelo/Libro.java
@@ -81,7 +81,7 @@ public class Libro {
                  "\nEditorial    : " + editorial +
                  "\nAño Publicación : " + anioPublicacion +
                  "\nGénero       : " + genero +
-                 "\n----------------------";
+                 "\n-";
 
     }
 }
--- src/main/java/Modelo/LibroElectronico.java
+++ src/main/java/Modelo/LibroElectronico.java
@@ -2,12 +2,14 @@ package Modelo;
 
 public class LibroElectronico extends Libro {
     private String formatoLibro;
+    private double tamanio;
     private String url;
 
     public LibroElectronico (String titulo, String autor,String isbn, String editorial, String anioPublicacion, String genero,
-                             String formatoLibro, String url){
+                             String formatoLibro, double tamanio, String url){
         super (titulo, autor, isbn, editorial, anioPublicacion, genero);
         this.formatoLibro = formatoLibro;
+        this.tamanio = tamanio;
         this.url = url;
     }
 
@@ -19,6 +21,14 @@ public class LibroElectronico extends Libro {
         this.formatoLibro = formatoLibro;
     }
 
+    public double getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(double tamanio) {
+        this.tamanio = tamanio;
+    }
+
     public String getUrl() {
         return url;
     }
@@ -28,9 +38,9 @@ public class LibroElectronico extends Libro {
     }
     @Override
     public String toString(){
-        return super.toString() + " | LibroElectronico"+
-        "formato del libro='" + formatoLibro + '\'' +
-                ", urlDescarga='" + url+ '\'' +
-                '}';
+        return super.toString() + "\nFormato del libro : " + formatoLibro +
+                "\nTamaño (MB)       : " + tamanio +
+                "\nEnlace            : " + url +
+                "\n---------------------------------";
     }
 }
