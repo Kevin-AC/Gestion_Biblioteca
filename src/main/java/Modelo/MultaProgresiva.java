1c9a21ddc48900b172f1e2a27cc894f88372616f
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-10-05 19:27:44 -0500

    Add:clase MultaProgresiva

---
+++ src/main/java/Modelo/MultaProgresiva.java
@@ -0,0 +1,12 @@
+package Modelo;
+
+public class MultaProgresiva implements CalcularMulta{
+    @Override
+    public double calcularMulta(int diasAtraso) {
+        double multa = 0.0;
+        for (int i = 1; i <= diasAtraso; i++) {
+            multa += i * 1.0; // Multa progresiva: $1 el primer día, $2 el segundo, etc.
+        }
+        return multa;
+    }
+}
