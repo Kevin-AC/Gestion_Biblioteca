4d65f41dc36129d5fcccf5350a413ca8254c1a85
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-10-05 19:26:59 -0500

    Add:clase MultaFija

---
+++ src/main/java/Modelo/MultaFija.java
@@ -0,0 +1,8 @@
+package Modelo;
+
+public class MultaFija implements CalcularMulta{
+    @Override
+    public double calcularMulta(int diasAtraso) {
+        return diasAtraso * 2.0; // Multa fija de $2 por día de retraso
+    }
+}
